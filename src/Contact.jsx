ae53c82445822068b0c9ee1041ff285aefad2bce
eskayML <kalusam619@gmail.com>
2023-08-22 13:00:05 +0100

    fix: contact form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,42 +1,41 @@
 import emailjs from '@emailjs/browser';
 import React, { useRef } from 'react';
 
-const SERVICE_ID ='service_8luy121';
+const SERVICE_ID = 'service_8luy121';
 const TEMPLATE_ID = 'template_769r3j6';
-const PUBLIC_KEY = 'MaThsTtrPVdWpsDka';
+const PUBLIC_KEY = 'HuHne3fsWTTd854xt';
 
+export default function Contact() {
+  const form = useRef();
 
-export default function Contact(){
-    const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    const sendEmail = (e) => {
-      e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+      .then((result) => {
+        console.log(result.text);
+        // Clear the form after successful submission
+        form.current.reset();
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
+  };
 
-  
-      emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
-    return (
-        <div id = 'Contact' className="container">
-            <div className="mt-5 mb-3 text-center">
-                <h3>Contact</h3>
-                <p>Let's talk about your project</p>
-                <form ref={form} onSubmit={sendEmail}>
-
-                    <input type="text" name="Name" placeholder="Your Name" />
-                    <input type="email" name="Email" id="" placeholder="Your Email" />
-                    <input type="text" name="Subject" placeholder="Subject"/>
-                    <textarea name="Content"  placeholder="Write Content.."></textarea>
-                   
-                    <button type="submit" className="btn btn-dark btn-long">Send</button>
-                  
-                </form>
-            </div>
-            
-        </div>
-    )
+  return (
+    <div id="Contact" className="container">
+      <div className="mt-5 mb-3 text-center">
+        <h3>Contact</h3>
+        <p>Let's talk about your project</p>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="from_name" placeholder="Your Name" />
+          <input type="email" name="from_email" placeholder="Your Email" />
+          <textarea name="message" placeholder="Write Message.."></textarea>
+          <button type="submit" className="btn btn-dark btn-long">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
